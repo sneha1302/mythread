eae77d8c1ccac09a176308ee47acc18d54917346
haithcockce <haithcockce@gmail.com>
2016-01-06 19:57:27 -0500

    My first OS program push! :D:D:D

---
+++ mythread.c
@@ -0,0 +1,41 @@
+void main(int* args) 
+{
+
+}
+
+/*
+ * creates the thread and puts it into ready queue
+ *
+ */
+MyThread MyThreadCreate (void(*start_funct)(void *), void *args);
+
+/*
+ * Puts the thread back on the wait queue
+ */
+void MyThreadYield(void);
+
+/*
+ * puts thread on join queue and wait for the joining thread to complete
+ */
+int MyThreadJoin(MyThread thread);
+
+
+void MyThreadJoinAll(void);
+
+/*
+ * Exists the thread
+ */
+void MyThreadExit(void);
+
+/*
+ * abstraction for synchronization. Creates a sem
+ */
+MySemaphore MySemaphoreInit(int initialValue);
+
+
+void MySemaphoreSignal(MySemaphore sem);
+
+
+void MySemaphoreWait(MySemaphore sem);
+int MySemaphoreDestroy(MySemaphore sem);
+void MyThreadInit (void(*start_funct)(void *), void *args);
